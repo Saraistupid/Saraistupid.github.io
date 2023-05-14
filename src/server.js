51732f47aadf01e92b190b8e1ad8a4d344ee50b0
Saraistupid <hoda48130@gmail.com>
2023-05-14 18:05:32 +0300

    should work idk

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+		'Saraistupid.github.io',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
